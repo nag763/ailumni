ad5c8402455fc1a3b189cbb66abd98a91377453a
nag763 <loic.labeye@tutanota.de>
2025-07-24 20:37:36 +0200

    faet: add vector db creation script

---
+++ terraform/vector.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "multiple_commands_create_bash" {
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    when = create
+    command = "aws s3vectors create-vector-bucket --vector-bucket-name 'ailumni-vector-db' && aws s3vectors create-index --vector-bucket-name 'ailumni-vector-db' --index-name 'ailumni-vector-index' --dimension 1048 --distance-metric 'cosine' --data-type float32"
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "aws s3vectors delete-index --vector-bucket-name 'ailumni-vector-db' --index-name 'ailumni-vector-index' && aws s3vectors delete-vector-bucket --vector-bucket-name ailumni-vector-db"
+  }
+}
