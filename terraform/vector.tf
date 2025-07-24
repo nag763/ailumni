9226fcbe5146c092311ff33d0559b2f104f76677
nag763 <loic.labeye@tutanota.de>
2025-07-24 20:38:57 +0200

    fix: vector size

--- terraform/vector.tf
+++ terraform/vector.tf
@@ -6,7 +6,7 @@ resource "null_resource" "multiple_commands_create_bash" {
 
   provisioner "local-exec" {
     when = create
-    command = "aws s3vectors create-vector-bucket --vector-bucket-name 'ailumni-vector-db' && aws s3vectors create-index --vector-bucket-name 'ailumni-vector-db' --index-name 'ailumni-vector-index' --dimension 1048 --distance-metric 'cosine' --data-type float32"
+    command = "aws s3vectors create-vector-bucket --vector-bucket-name 'ailumni-vector-db' && aws s3vectors create-index --vector-bucket-name 'ailumni-vector-db' --index-name 'ailumni-vector-index' --dimension 1024 --distance-metric 'cosine' --data-type float32"
   }
 
   provisioner "local-exec" {
