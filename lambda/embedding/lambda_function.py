31bccc7a71832ecaf60f2a536e337d120d90aa34
nag763 <loic.labeye@tutanota.de>
2025-07-24 19:09:49 +0200

    feat: embedding of documents

--- lambda/embedding/lambda_function.py
+++ lambda/embedding/lambda_function.py
@@ -9,6 +9,8 @@ aws_region = os.getenv("AWS_REGION", "eu-central-1")
 
 bedrock = boto3.client("bedrock-runtime", region_name=aws_region)
 
+s3vectors = boto3.client("s3vectors", region_name="eu-central-1")
+
 def lambda_handler(event, context):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -26,87 +28,26 @@ def lambda_handler(event, context):
     content = response["Body"].read().decode("utf-8")
 
     logger.info(f"Content of {key}: {content}")
-    # embeddings = []
-    # # Generate vector embeddings for the input texts
-    # for text in texts:
-    #     body = json.dumps({"inputText": text})
-    #     # Call Bedrock's embedding API
-    #     response = bedrock.invoke_model(
-    #         modelId="amazon.titan-embed-text-v2:0", body=body  # Titan embedding model
-    #     )
-    #     # Parse response
-    #     response_body = json.loads(response["body"].read())
-    #     embedding = response_body["embedding"]
-    #     embeddings.append(embedding)
-
-    # # Create S3Vectors client
-
-    # # # Insert vector embedding
-    # s3vectors.put_vectors(
-    #     vectorBucketName="ailumni-vector-db",
-    #     indexName="ailumni-vector-index",
-    #     vectors=[
-    #         {
-    #             "key": "v1",
-    #             "data": {"float32": embeddings[0]},
-    #             "metadata": {"id": "key1", "source_text": texts[0], "genre": "scifi"},
-    #         },
-    #         {
-    #             "key": "v2",
-    #             "data": {"float32": embeddings[1]},
-    #             "metadata": {"id": "key2", "source_text": texts[1], "genre": "scifi"},
-    #         },
-    #         {
-    #             "key": "v3",
-    #             "data": {"float32": embeddings[2]},
-    #             "metadata": {"id": "key3", "source_text": texts[2], "genre": "family"},
-    #         },
-    #         {
-    #             "key": "v4",
-    #             "data": {"float32": embeddings[3]},
-    #             "metadata": {"id": "key4", "source_text": texts[3], "genre": "crime"},
-    #         },
-    #     ],
-    # )
-
-    # Get the search query from CLI argument
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <search_query>")
-        sys.exit(1)
-    input_text = sys.argv[1]
-
-    # Create the JSON request for the model.
-    request = json.dumps({"inputText": input_text})
-
-    # Invoke the model with the request and the model ID, e.g., Titan Text Embeddings V2.
+    body = json.dumps({"inputText": content})
+    # Call Bedrock's embedding API
     response = bedrock.invoke_model(
-        modelId="amazon.titan-embed-text-v2:0", body=request
+        modelId="amazon.titan-embed-text-v2:0", body=body  # Titan embedding model
     )
+    # Parse response
+    response_body = json.loads(response["body"].read())
+    embedding = response_body["embedding"]
 
-    # Decode the model's native response body.
-    model_response = json.loads(response["body"].read())
+    # Create S3Vectors client
 
-    # Extract and print the generated embedding and the input text token count.
-    embedding = model_response["embedding"]
-
-    # Performa a similarity query. You can also optionally use a filter in your query
-    query = s3vectors.query_vectors(
+    # # Insert vector embedding
+    s3vectors.put_vectors(
         vectorBucketName="ailumni-vector-db",
         indexName="ailumni-vector-index",
-        queryVector={"float32": embedding},
-        topK=3,
-        returnDistance=True,
-        returnMetadata=True,
+        vectors=[
+            {
+                "key": key,
+                "data": {"float32": embedding},
+                "metadata": {"id": key, "source_text": content},
+            },
+        ],
     )
-    results = query["vectors"]
-    # Print the similarity query results in a formatted way
-    for i, result in enumerate(results, 1):
-        print(f"Result {i}:")
-        print(f"  Key: {result.get('key')}")
-        print(f"  Distance: {result.get('distance')}")
-        print(f"  Metadata: {json.dumps(result.get('metadata'), indent=2)}")
-        print()
-
-
-if __name__ == "__main__":
-    main()
