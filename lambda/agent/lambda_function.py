e749424737ba0962b1166fa640cb4863d88eae5a
nag763 <loic.labeye@tutanota.de>
2025-07-25 19:02:00 +0200

    fix: logging

--- lambda/agent/lambda_function.py
+++ lambda/agent/lambda_function.py
@@ -5,14 +5,18 @@ import logging
 
 cors_allow_origin = os.getenv("CORS_ALLOW_ORIGIN", "*")
 
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
 def lambda_handler(event, context):
-    logging.info("Received event: %s", json.dumps(event))
+    logger.info("Received event: %s", json.dumps(event))
     http_method = event.get("httpMethod", "GET")
-    logging.info("HTTP method: %s", http_method)
+    logger.info("HTTP method: %s", http_method)
 
     match http_method:
         case "OPTIONS":
-            logging.info("Handling OPTIONS request")
+            logger.info("Handling OPTIONS request")
             return {
                 "statusCode": 204,
                 "headers": {
@@ -22,7 +26,7 @@ def lambda_handler(event, context):
                 }
             }
         case "POST":
-            logging.info("Handling POST request")
+            logger.info("Handling POST request")
             return {
                 "statusCode": 200,
                 "body": json.dumps({
@@ -30,7 +34,7 @@ def lambda_handler(event, context):
                 })
             }
         case _:
-            loggingger.warning("Unhandled HTTP method: %s", http_method)
+            logger.warning("Unhandled HTTP method: %s", http_method)
             return {
                 "statusCode": 405,
                 "body": json.dumps({"error": "Method Not Allowed"})
