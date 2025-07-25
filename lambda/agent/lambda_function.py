37fd54c9162f620c754932fab1dc5aff9dbed65e
nag763 <loic.labeye@tutanota.de>
2025-07-25 18:58:08 +0200

    feat: add logging in lambda

--- lambda/agent/lambda_function.py
+++ lambda/agent/lambda_function.py
@@ -1,12 +1,18 @@
 import json
 import os
+import logging
+
 
 cors_allow_origin = os.getenv("CORS_ALLOW_ORIGIN", "*")
 
 def lambda_handler(event, context):
+    logging.info("Received event: %s", json.dumps(event))
     http_method = event.get("httpMethod", "GET")
+    logging.info("HTTP method: %s", http_method)
+
     match http_method:
         case "OPTIONS":
+            logging.info("Handling OPTIONS request")
             return {
                 "statusCode": 204,
                 "headers": {
@@ -16,9 +22,16 @@ def lambda_handler(event, context):
                 }
             }
         case "POST":
+            logging.info("Handling POST request")
             return {
                 "statusCode": 200,
                 "body": json.dumps({
                     "message": "Hello from the agent lambda function!"
                 })
+            }
+        case _:
+            logloggingger.warning("Unhandled HTTP method: %s", http_method)
+            return {
+                "statusCode": 405,
+                "body": json.dumps({"error": "Method Not Allowed"})
             }
